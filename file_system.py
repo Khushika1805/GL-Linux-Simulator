e47ee5a4cb3f28ce5401f6377c494c6891f9c05b
Khushika H Shah <123218895+Khushika1805@users.noreply.github.com>
2025-02-14 20:44:48 -0500

    Add files via upload

---
+++ file_system.py
@@ -0,0 +1,287 @@
+Last login: Fri Feb 14 20:34:15 on ttys002
+khushikashah@Khushikas-Laptop-8 ~ % ls
+Applications			Music
+Desktop				Pictures
+Documents			Public
+Downloads			matlab_crash_dump.26900-1
+Library				matlab_crash_dump.90541-1
+Movies
+khushikashah@Khushikas-Laptop-8 ~ % rm matlab_crash_dump.26900-1
+khushikashah@Khushikas-Laptop-8 ~ % rm matlab_crash_dump.90541-1
+khushikashah@Khushikas-Laptop-8 ~ % ls
+Applications	Documents	Library		Music		Public
+Desktop		Downloads	Movies		Pictures
+khushikashah@Khushikas-Laptop-8 ~ % cd Downloads
+khushikashah@Khushikas-Laptop-8 Downloads % ls
+00. Linux Kernel Tutorial for Beginners.pdf
+01 Call-Center-Dataset.xlsx
+11-4_393.pptx
+1721858356477.heic
+2024 vision
+2024 vision.png
+202410201848.mp4
+202412152313.mp4
+31.png
+374, P-9.xlsx
+441.goodnotes
+441.pdf
+A 112-18 Chapter 18 Lecture Notes -- Managerial Accounting Word doc.docx
+A Super Cyber Story () (4).png
+AdaaFall24-042_Original.jpg
+Assignment #1 Sanity Check.pdf
+Backup 2024-09-26
+BatMan.png
+Be Your Best Self in Real Life Paper #1.pdf
+Book1.pdf
+CV - Schonfield.pdf
+Copy of Decore_More_dataset_MA.xlsx
+Copy of SPXchartingCTAS.xls
+Copy of chartingExcelCTAS.xls
+Cover letter - Penguin Random House.pdf
+Customized Team Working Agreement.docx
+D.E Shaw CL.pdf
+DS_Store
+Daily T-bill interest rates for June 2023.xlsx
+Decore_More_dataset_MA.xlsx
+Downloads
+Econ374-Part1.xlsx
+Elm Ave.m4a
+Exam 1-2.pdf
+Exam 1.pdf
+Exam 3
+Fall 2024 Escape Room
+FamilyInsurance_Data_MA_Alt1_Problem.xlsx
+Files from Khushika Shah.eml
+Final Exam.pdf
+Final Reflection.pdf
+Finding the differences that make a difference-1.pdf
+GDSC_Pathways_to_Profession_Success.pptx
+Gather Your Thoughts Paper #2.pdf
+Gather Your Thoughts Paper #5.pdf
+Gather Your Thoughts Paper #6.pdf
+Gothic-Joker.ttf
+HW #1.pdf
+HW #4-2.pdf
+HW #4.pdf
+HW #5.pdf
+HW #6.pdf
+IMG_0154.heic
+IMG_0155.heic
+IMG_1478.HEIC
+IMG_6630 2.PNG
+IMG_6630.PNG
+IMG_9569.HEIC
+IMG_9575.HEIC
+IMG_9894.heic
+IMG_9895.heic
+Install Spotify.app
+Instructions Assignment.docx
+Instructions Assignment.pdf
+Instructions Peer Review.pdf
+Introduction.to.Algorithms.4th.Leiserson.Stein.Rivest.Cormen.MIT.Press.9780262046305.EBooksWorld.ir.pdf
+JP Morgan Audit Internship.pdf
+JP Morgan Internship.pdf
+Jake_s_Resume-2.pdf
+Jake_s_Resume.pdf
+Jake_s_Resume__1_-2.pdf
+Jake_s_Resume__1_.pdf
+Keeper Entries.pdf
+Khushika Shah.png
+Levels of Nothing -  Robert Lawrence Kuhn - Skeptic magazine - 2013.pdf
+M2_ Critique of the Cultural Iceberg  Assignment.pdf
+M3_ Listen and Reply Podcast.pdf
+M4_ Activity_ Increasing Empathy .pdf
+M6_ Personal Reflection DIFUB.pdf
+MicrosoftTeams_s_8DD4C59330202C1-3-0_c_w_.pkg
+New year’s aesthetic.jpg
+Nominalization.ppt
+Notes.docx
+OLS_in_Excel.xlsx
+Opinion _ How to Fall in Love With Math - The New York Times.pdf
+Parable Reaction.pdf
+Part 2 template.numbers
+Part2.xlsx
+Peer Review.pdf
+Proj pt 2. W.W Grainger resubmission 1-2.xlsx
+Proj pt 2. W.W Grainger resubmission 1.xlsx
+Project instructions.docx
+RA.docx
+Reflection #5.pdf
+Reflection #6.pdf
+Rhetorical Analysis Assignment
+Rhetorical Analysis Assignment-2.pdf
+Rhetorical Analysis Assignment-3.pdf
+Rhetorical Analysis Assignment.docx
+Rhetorical Analysis Assignment.pdf
+Rihanna - We Found Love ft. Calvin Harris.mp3
+SPXchartingCTAS.xls
+SSR_TSRPT-2.pdf
+SSR_TSRPT-3.pdf
+SSR_TSRPT-4.pdf
+SSR_TSRPT-5.pdf
+SSR_TSRPT.pdf
+SZA - BMF (Official Audio).mp3
+Sample_instructions2.docx
+Skills - Excel Template.xlsx
+Smoke Detector Test.mp3
+Sonic Warning Assignment-2.pdf
+Sonic Warning Assignment.pdf
+SpotifyInstaller.zip
+Spring 2025 HONR Electives.docx
+Spring-25-UGRD-Calendar.pdf
+Stata18.dmg
+Study-Guide.pdf
+Syllabus CMSC 421.docx
+Task 1 - Email Template v2.docx
+Task 2 Template.pptx
+Template for students -- Part 1 template.xlsx
+Template for students -- Part 2 template.xlsx
+Template for students -- Part 3 template.xlsx
+The Linux System.pdf
+TimeValueMoney_TI83_Act13.pdf
+Untitled document.pdf
+VSCode-darwin-universal.zip
+Values Americans Live By.pdf
+Visual Studio Code 2.app
+WhatsApp-2.24.16.80.dmg
+Zoom-2.pkg
+Zoom.pkg
+_OceanofPDF.com_The_Big_Bang_of_Numbers_-_Manil_Suri.pdf
+appWalkthrough.gif
+appWalkthrough.mov
+chartingExcelCTAS.xls
+csprd-web.ps.umbc.edu:.webloc
+csprd-web.ps.umbc.edu_.webloc
+dotconfig
+download.html
+eTicketYSGVLX.pdf
+econ-374-part-2.xlsx
+git-cheat-sheet-education.pdf
+iceland vlog
+main.c
+matlab_R2024a_macOSAppleSilicon.dmg
+mycpuwcomparatormoksh.slx
+mycpuwithtimer1.slx
+north.jpg
+of yapping
+ols_in_excel.pdf
+part3-ECON374.xlsx
+project1
+python-3.12.4-macos11.pkg
+reflection4.docx
+sound.docx
+videoplayback.mp4
+khushikashah@Khushikas-Laptop-8 Downloads % cd ..
+khushikashah@Khushikas-Laptop-8 ~ % ls
+Applications	Documents	Library		Music		Public
+Desktop		Downloads	Movies		Pictures
+khushikashah@Khushikas-Laptop-8 ~ % ssh kshah7@gl.umbc.edu
+WARNING: UNAUTHORIZED ACCESS to this computer is in violation of Criminal Law
+         Article section 8-606 and 7-302 of the Annotated Code of MD.
+
+NOTICE:  This system is for the use of authorized users only. Individuals using
+         this computer system without authority, or in excess of their 
+         authority, are subject to having all of their activities on this system
+         monitored and recorded by system personnel.
+
+(kshah7@gl.umbc.edu) Password: 
+(kshah7@gl.umbc.edu) Duo two-factor login for kshah7
+
+Enter a passcode or select one of the following options:
+
+ 1. Duo Push to XXX-XXX-0585
+ 2. Phone call to XXX-XXX-0585
+ 3. SMS passcodes to XXX-XXX-0585
+
+Passcode or option (1-3): 1
+Success. Logging you in...
+Success. Logging you in...
+
+UMBC Division of Information Technology                    http://doit.umbc.edu/
+--------------------------------------------------------------------------------
+If you have any questions or problems regarding these systems, please call the
+DoIT Technology Support Center at 410-455-3838, or submit your request on the
+web by visiting http://my.umbc.edu/help/request
+
+Remember that the Division of Information Technology will never ask for your
+password. Do NOT give out this information under any circumstances.
+--------------------------------------------------------------------------------
+
+Last login: Fri Feb 14 20:34:41 2025 from 71.179.255.4
+[kshah7@linux6 ~]$ ls
+201_Grading  203  cmsc201  cs341      myProj
+202          331  cmsc313  cs341proj  practice
+[kshah7@linux6 ~]$ cd cmsc201
+[kshah7@linux6 ~/cmsc201]$ ls
+GL-Linux-Simulator  Homeworks  Labs  project1  projects  TA
+[kshah7@linux6 ~/cmsc201]$ cd projects
+[kshah7@linux6 projects]$ cd project3
+[kshah7@linux6 project3]$ ls
+damn.py  file_system_example.pyc  file_system.py
+[kshah7@linux6 project3]$ emacs file_system.py
+
+File Edit Options Buffers Tools Python Help                                                                                                                                                                     
+'''                                                                                                                                                                                                             
+File: file_system.py                                                                                                                                                                                            
+Author: Khushika Shah                                                                                                                                                                                           
+Date: 12/1/2022                                                                                                                                                                                                 
+Section: #26                                                                                                                                                                                                    
+Email: kshah7@umbc.edu                                                                                                                                                                                          
+Description: Create a program that works as a simulator for the linux system                                                                                                                                    
+'''
+
+# GLOBAL CONSTANTS:                                                                                                                                                                                             
+ROOT = '/'
+DIR_SEP = '/'
+LAST_DIR = '..'
+COMMAND_LS = 'ls'
+COMMAND_PWD = 'pwd'
+COMMAND_TOUCH = 'touch'
+COMMAND_MKDIR = 'mkdir'
+COMMAND_RM = 'rm'
+COMMAND_LOCATE = 'locate'
+COMMAND_CD = 'cd'
+COMMAND_EXIT = 'exit'
+
+
+def run_the_file_system():
+    '''                                                                                                                                                                                                         
+    :Description: main function for all code! will ask user for input and simulate the gl linux system                                                                                                          
+    :return: what user wants                                                                                                                                                                                    
+    '''
+
+    # ask user for input                                                                                                                                                                                        
+    enter = input('[cmsc201 proj3]$ ')
+
+    # sets file_system and our root directory will be '/'                                                                                                                                                       
+    file_system = {ROOT: {}}
+
+    # DIR is to add root directory to path                                                                                                                                                                      
+    DIR = ['/']
+
+    # when user enters exit, while loop and the program ends                                                                                                                                                    
+    while enter != COMMAND_EXIT:
+
+        enter_split = enter.split()
+
+        # the command the user puts                                                                                                                                                                             
+        command = enter_split[0]
+        # subject of the command:                                                                                                                                                                               
+        directory_or_file = enter_split[-1]
+        # make path into list                                                                                                                                                                                   
+        path = directory_or_file.split('/')
+
+        # if path first item is '', this removes it                                                                                                                                                             
+        if path[0] == '':
+            del path[0]
+
+        # initializing path                                                                                                                                                                                     
+        path = DIR
+
+        # gets current directory                                                                                                                                                                                
+        current_dir = tracker(file_system, path)
+
+        # this command prints out contents of the directory the user chooses to look into                                                                                                                       
+        if command == COMMAND_LS:
+-UU-:---  F1  file_system.py   Top   L1     (Python ElDoc) -----------------------------------------------------------------------------------------------------------------------------------------------------
+
